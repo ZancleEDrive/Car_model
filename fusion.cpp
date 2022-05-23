3597c84ec2f739cf308e47137f5a5fe121df81f2
Lomby1902 <85572398+Lomby1902@users.noreply.github.com>
2022-05-23 15:54:06 +0200

    Fusione dati accelerometro e ruote. Problema per velocità y

--- fusion.cpp
+++ fusion.cpp
@@ -106,6 +106,12 @@ float calcola_velocita(float acc,float tempo){
 
 }
 
+//calcola l'accelerazione derivando il valore dell'accelerazione, 
+//rispetto solamente al tempo trascorso per effettuare la misura.
+float calcola_accelerazione(float vel, float tempo){
+  float acc=vel/tempo
+}
+
 
 
 float filtro_kalman(float stima,float errore, float errore_misura,float misura){
@@ -128,12 +134,12 @@ float filtro_kalman(float stima,float errore, float errore_misura,float misura){
     //accelerazione
     float acc_x=0;
     float acc_y=0;
-    float acc_prec_x=0;
-    float acc_prec_y=0;
+ 
     
     //velocità
     float v_x=0;
     float v_y=0;
+
 
     //l'angolo di inclinzione si suppone costante quindi il
     //disturbo dovrà rimanere lo stesso
@@ -141,6 +147,94 @@ float filtro_kalman(float stima,float errore, float errore_misura,float misura){
     double disturbo_y=0;
 
  
+
+
+
+
+
+
+
+
+
+
+
+const int fori = 20;
+int counter = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+InterruptIn sensore(ARDUINO_UNO_D3);
+InterruptIn sensore1(ARDUINO_UNO_D2);
+InterruptIn sensore2(ARDUINO_UNO_D4);
+InterruptIn sensore3(ARDUINO_UNO_D5);
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+DigitalOut led3(LED3);
+//DigitalOut flash();
+
+
+void Rpm()
+{
+    led1 = !led1;
+    counter++;
+}
+
+void Rpm1()
+{
+    led2 = !led2;
+    counter1++;
+}
+
+
+void Rpm2()
+{
+    led3 = !led3;
+    counter2++;
+}
+
+
+
+void Rpm3()
+{
+    counter3++;
+}
+
+double get_speed
+
+
+//Restituisce il valore della velocità dagli rpm
+float converti_rpm(int counter){
+    float raggio;
+    return raggio*(2*PI/60)*counter;
+
+
+}
+
+
+int main(){
+    // attach the address of the flip function to the rising edge
+    //sensore1.rise(&Rpm1);
+    while (1) {          // wait around, interrupts will interrupt this!
+      
+       // ThisThread::sleep_for(250ms);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* Simple main function */
 int main() {
   uint8_t id;
@@ -153,6 +247,10 @@ int main() {
   accelerometer->read_id(&id);
   accelerometer->get_x_axes(axes);
 
+  sensore3.rise(&Rpm3);
+  sensore2.rise(&Rpm2);
+  sensore1.rise(&Rpm1);
+  sensore.rise(&Rpm); 
  
   //calcola il disturbo all'inzio sapendo che il valore
   //al tempo 0 segnato dall'accelerometro deve essere 0.
@@ -186,20 +284,51 @@ int main() {
     acc_x=acc_x-disturbo_x;
     acc_y=acc_y-disturbo_y;
 
+   
+
+
+    // led = !led
+    ThisThread::sleep_for(250ms);
+
+    //velocità lineare delle singole ruote
+    float vel1=converti_rpm(counter*12)
+    float vel2=converti_rpm(counter1*12)
+    float vel3=converti_rpm(counter2*12)
+    float vel4=converti_rpm(counter3*12)
+
+    //media delle velocità
+    float vel=(vel1+vel2+ vel3 + vel4)/4;
+
+    printf("%d rpm  \t", counter*12);
+    printf("%d rpm1 \t", counter1*12);
+    printf("%d rpm2 \t", counter2*12);
+    printf("%d rpm3 \n", counter3*12);
+    printf("v[m/s]: %f\n",vel)
+    counter3=0;
+    counter2=0;
+    counter1=0;
+    counter=0;
+    
+    //calcola l'accelerazione a partire dalla velocità delle ruote
+    acc_stimata_x=calcola_accelerazione(vel,elapsedTime_misura)
+    
     //applica il filtro di Kalman all'accelerazione
-    acc_x=filtro_kalman(acc_prec_x,0.02,0.03,acc_x);
-    acc_y=filtro_kalman(acc_prec_y,0.01,0.01,acc_y);
+    //usando il dato delle ruote come stima
+    acc_x_filtrata=filtro_kalman(acc_stimata_x,0.02,0.03,acc_x);
+    acc_y_filtrata=filtro_kalman(acc_prec_y,0.01,0.01,acc_y);
+    
+    //calcola la velocità a partire dall'accelerometro
+    vel_x_stimata=calcola_velocita(acc_x,elapsedTime_misura);
+    vel_y_stimata=calcola_velocita(acc_y,elapsedTime_misura);
+    
+    //applica il filtro di Kalman alla velocità
+    //usando il dato dell'accelerometro come stima
+    vel_x_filtrata=filtro_kalman(vel_x_stimata,0.02,0.03,vel);
+    //vel_y_filtrata=filtro_kalman(vel_y_stimata,0.01,0.01,acc_y);
+    
 
-    //calcola la velocità
-    v_x=calcola_velocita(acc_x,elapsedTime_misura);
-    v_y=calcola_velocita(acc_y,elapsedTime_misura);
-
-    acc_prec_x=acc_x;
-    acc_prec_y=acc_y;
-
-
-    printf("LSM303AGR [acc/mg]:  %6f, %6f\r\n", acc_x, acc_y);
-    printf("LSM303AGR [v(m/s)]:  %6f, %6f\r\n", v_x, v_y);
+    printf("LSM303AGR [acc/mg]:  %6f, %6f\r\n", acc_x_filtrata, acc_y_filtrata);
+    printf("LSM303AGR [v(m/s)]:  %6f, %6f\r\n", v_x_filtrata, v_y_stimata);
    
    ThisThread::sleep_for(1000);
 
